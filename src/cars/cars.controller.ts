c46f0d9509ce44a7963e46a40d0a6ead5c6804ee
Thomassr <thomassoto94@gmail.com>
2022-07-23 14:20:36 -0400

    eliminaciòn pretier y peticion get

--- src/cars/cars.controller.ts
+++ src/cars/cars.controller.ts
@@ -1,11 +1,18 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Param } from '@nestjs/common';
 
 @Controller('cars')
 export class CarsController {
 
+    private cars = ['Toyota', 'Honda', 'Jeep']
+
     @Get()
     getAllCars(){
-        return ['Toyota', 'Honda', 'Jeep']
+        return this.cars
+    }
+
+    @Get(':id')
+    getCarById(@Param('id') id ){
+        return this.cars[id - 1]
     }
 
 }
