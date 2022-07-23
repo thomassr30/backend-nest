4a130ad249d476e221d515f247b1cfb8d8e8d247
Thomassr <thomassoto94@gmail.com>
2022-07-23 17:35:28 -0400

    pipes

--- src/cars/cars.controller.ts
+++ src/cars/cars.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get, Param } from '@nestjs/common';
+import { Controller, Get, Param, ParseIntPipe } from '@nestjs/common';
 import { CarsService } from './cars.service';
 
 @Controller('cars')
@@ -14,7 +14,7 @@ export class CarsController {
     }
 
     @Get(':id')
-    getCarById(@Param('id') id ){
+    getCarById(@Param('id', ParseIntPipe) id ){
         return this.carsService.findCardById(id)
     }
 
