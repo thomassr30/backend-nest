1f49dfa99fb9b39a551b5664a799447aaaa02a09
Thomassr <thomassoto94@gmail.com>
2022-07-23 18:03:41 -0400

    POST, PATCH, DELETE

--- src/cars/cars.controller.ts
+++ src/cars/cars.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get, Param, ParseIntPipe } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, ParseIntPipe, Patch, Post } from '@nestjs/common';
 import { CarsService } from './cars.service';
 
 @Controller('cars')
@@ -16,6 +16,24 @@ export class CarsController {
     @Get(':id')
     getCarById(@Param('id', ParseIntPipe) id ){
         return this.carsService.findCardById(id)
+    }
+
+    @Post()
+    createCar(@Body() body: any){
+        return body
+    }
+
+    @Patch(':id')
+    updateCar(@Param('id', ParseIntPipe) id,@Body() body: any){
+        return body
+    }
+
+    @Delete(':id')
+    deleteCar(@Param(':id', ParseIntPipe) id){
+        return {
+            method: 'DELETE',
+            id
+        }
     }
 
 }
