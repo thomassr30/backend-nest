c473e1af9545b79757cfb839152b3050536372ab
Thomassr <thomassoto94@gmail.com>
2022-07-23 17:43:53 -0400

    Exception Filters

--- src/cars/cars.service.ts
+++ src/cars/cars.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 
 @Injectable()
 export class CarsService {
@@ -26,7 +26,14 @@ export class CarsService {
     }
 
     findCardById(id){
-        return this.cars[id-1]
+
+        const car = this.cars.find(car => car.id === id);
+        
+        if(!car){
+            throw new NotFoundException(`Car with id ${id} not found`)
+        }
+
+        return car
     }
 
 }
