bec7c182d25b197cf94b273fd0015f3a3ea76696
Thomassr <thomassoto94@gmail.com>
2022-07-23 14:06:59 -0400

    Eliminación de archivos y creacion de module y controller cars

---
+++ src/cars/cars.controller.ts
@@ -0,0 +1,11 @@
+import { Controller, Get } from '@nestjs/common';
+
+@Controller('cars')
+export class CarsController {
+
+    @Get()
+    getAllCars(){
+        return ['Toyota', 'Honda', 'Jeep']
+    }
+
+}
